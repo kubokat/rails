53b79589ca8bf80f8fd22caa5d8bec2e6a253221
vladislavChe <kubokat@gmail.com>
2019-02-28 02:14:21 +0500

    part4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "User 1")
+Category.create([{title: "first category"}, {title: "last category"}])
+Test.create([{title: "Test1", level: 1, category_id: 1}, {title: "Test2", level: 1, category_id: 1}, {title: "Test3", level: 2, category_id: 2}])
+
+Test.all.each do |test|
+  test.users << User.first
+end
+
+questions = []
+
+(0..15).each do |number|
+  questions[number] = {body: "question number #{number+1}", test_id: 1}
+end
+
+Question.create(questions)
+
+Answer.create(correct: false, question_id: 1)
