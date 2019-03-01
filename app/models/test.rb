22310d9b183ee6de8f578c507bba1250cbc01393
vladislavChe <kubokat@gmail.com>
2019-03-01 13:22:01 +0500

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :category
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
 
   def self.names_by_category(name)
     self.joins(:category).where(title: name).order('tests.title DESC').pluck('tests.title')
