d0ef0df2d77c47c5a49d91a58c9dc465122f2d03
vladislavChe <kubokat@gmail.com>
2019-03-16 19:42:22 +0500

    fix

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,13 @@
 module QuestionsHelper
-  def question_header(method, title)
-    "#{method.capitalize} New #{title} Question"
+  def question_header(question)
+
+    if question.new_record?
+      action = :create
+    else
+      action = :edit
+    end
+
+    "#{action.capitalize} New #{question.test.title} Question"
   end
 
   def current_year
@@ -8,6 +15,6 @@ module QuestionsHelper
   end
 
   def github_url(author, repo)
-    link_to 'Test-Guru', "https://github.com/#{author}/#{repo}"
+    link_to 'Test-Guru', "https://github.com/#{author}/#{repo}", target: :_blank
   end
 end
