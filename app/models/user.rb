1cc90db6403d76d5692fe6f176c94a4e8b6d123b
vladislavChe <kubokat@gmail.com>
2019-04-02 00:30:44 +0500

    part11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test", foreign_key: "author_id"
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  has_secure_password
 
   def tests_by_level(level)
     tests.where(level: level)
