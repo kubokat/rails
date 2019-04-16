9938576b26ee9d9870bc6dcc31c01d052dde8485
vladislavChe <kubokat@gmail.com>
2019-04-16 21:02:23 +0500

    part16 fix

--- app/assets/javascripts/utilites/progress.js
+++ app/assets/javascripts/utilites/progress.js
@@ -5,8 +5,6 @@ document.addEventListener('turbolinks:load', function () {
         var total = control.dataset.total;
         var number = control.dataset.number;
 
-        console.log((number / total) * 100)
-
         control.style.width = (number / total) * 100 + '%';
     }
 });
