1010159f7031560cc32b65509a76de42bd960f41
vladislavChe <kubokat@gmail.com>
2019-04-18 00:10:51 +0500

    fix

--- db/migrate/20190226101704_create_answers.rb
+++ db/migrate/20190226101704_create_answers.rb
@@ -1,7 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
-      t.integer :correct
+      t.boolean :correct
+      t.string :body
       t.references :question, foreign_key: true
 
       t.timestamps
