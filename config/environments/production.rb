fe267c7d0be3d4dfdbce1e4265fba9a9573bff73
vladislavChe <kubokat@gmail.com>
2019-04-22 22:11:56 +0500

    part 17 add smtp host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,17 +62,19 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test-guru_#{Rails.env}"
-
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'gentle-brook-39674.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 547,
-      user_name:            ENV['SMTP_USERNAME'],
-      password:             ENV['SMTP_PASSWORD'],
-      authentication:       'plain',
-      eneble_starttls_auto: true
+      port:                 587,
+      domain:               "gentle-brook-39674.herokuapp.com",
+      authentication:       "plain",
+      user_name:            ENV["SMTP_USERNAME"],
+      password:             ENV["SMTP_PASSWORD"],
+      enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
